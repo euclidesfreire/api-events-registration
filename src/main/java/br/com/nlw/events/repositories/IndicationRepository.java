6313f0c6ae8171e5a74f3c22bcc14a696ff00f19
euclides <euclidesfreiresa@gmail.com>
2025-02-27 23:14:00 -0300

    fix: attribute indication repository

--- src/main/java/br/com/nlw/events/repositories/IndicationRepository.java
+++ src/main/java/br/com/nlw/events/repositories/IndicationRepository.java
@@ -9,6 +9,6 @@ import br.com.nlw.events.models.Subscription;
 import br.com.nlw.events.models.User;
 
 public interface IndicationRepository extends CrudRepository<Indication, Integer> {
-        public Optional<Indication> findBySubscriptionAndUser(Subscription subscription, User user);
+        public Optional<Indication> findBySubscriptionIndicationAndUser(Subscription subscription, User user);
 
 }
