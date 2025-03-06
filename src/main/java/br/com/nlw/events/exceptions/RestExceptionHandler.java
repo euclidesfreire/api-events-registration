2fbae316fefc1308a42274ee62d4c30435f20517
euclides <euclidesfreiresa@gmail.com>
2025-03-06 13:25:26 -0300

    feat: create global exception RestExceptionHandler

---
+++ src/main/java/br/com/nlw/events/exceptions/RestExceptionHandler.java
@@ -0,0 +1,82 @@
+package br.com.nlw.events.exceptions;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import br.com.nlw.events.dto.ExceptionDTO;
+
+/**
+ * Global Controller Exception
+ * 
+ * com RestControllerAdvice
+ * 
+*/
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    /**
+     * Generic exception for Exception.class
+     * 
+     * @param Exception.class
+     * @return ResponseEntity exceptionDTO
+     * @return ResponseEntity HttpStatus.INTERNAL_SERVER_ERROR
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionDTO> genericException(Exception e){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(
+                                        HttpStatus.INTERNAL_SERVER_ERROR.value(), 
+                                        HttpStatus.INTERNAL_SERVER_ERROR.name(), 
+                                        List.of(e.getMessage()), 
+                                        LocalDateTime.now()
+                                        );
+
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    /**
+     * Not Found Exception
+     * 
+     * @param RuntimeException.class
+     * @return ResponseEntity exceptionDTO
+     * @return ResponseEntity HttpStatus.NOT_FOUND
+     */
+    @ExceptionHandler({
+        NotFoundException.class
+    })
+    public ResponseEntity<ExceptionDTO> notFoundException(RuntimeException e){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(
+                                        HttpStatus.NOT_FOUND.value(), 
+                                        HttpStatus.NOT_FOUND.name(), 
+                                        List.of(e.getMessage()), 
+                                        LocalDateTime.now()
+                                        );
+
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * Already Exists Exception
+     * 
+     * @param RuntimeException.class
+     * @return ResponseEntity exceptionDTO
+     * @return ResponseEntity HttpStatus.CONFLICT
+     */
+    @ExceptionHandler({
+        AlreadyExistsException.class
+    })
+    public ResponseEntity<ExceptionDTO> alreadyExistsException(RuntimeException e){
+        ExceptionDTO exceptionDTO = new ExceptionDTO(
+                                        HttpStatus.CONFLICT.value(), 
+                                        HttpStatus.CONFLICT.name(), 
+                                        List.of(e.getMessage()), 
+                                        LocalDateTime.now()
+                                        );
+
+        return new ResponseEntity<>(exceptionDTO, HttpStatus.CONFLICT);
+    }
+}
