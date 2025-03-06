eb7641bcf4d78ea10114b04734c5e5d63c8bfcb7
euclides <euclidesfreiresa@gmail.com>
2025-03-06 13:57:19 -0300

    edit: ajustes exceptions em services

--- src/main/java/br/com/nlw/events/controllers/EventController.java
+++ src/main/java/br/com/nlw/events/controllers/EventController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.nlw.events.exceptions.NotFoundException;
 import br.com.nlw.events.models.Event;
 import br.com.nlw.events.services.EventService;
 
--- src/main/java/br/com/nlw/events/services/EventService.java
+++ src/main/java/br/com/nlw/events/services/EventService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.nlw.events.exceptions.AlreadyExistsException;
 import br.com.nlw.events.exceptions.NotFoundException;
 import br.com.nlw.events.models.Event;
 import br.com.nlw.events.repositories.EventRepository;
@@ -29,7 +30,7 @@ public class EventService {
 
         //If Event Already Exists
         eventRepository.findByPrettyName(eventNew.getPrettyName())
-        .ifPresent(event -> { throw new NotFoundException("Event Already Exists: " + event.getPrettyName()); });
+        .ifPresent(event -> { throw new AlreadyExistsException("Event Already Exists: " + event.getPrettyName()); });
 
         Event event = eventRepository.save(eventNew);
 
